de6b1a1cf5f82463fdfd19790863df191cf66b7b
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-02 16:41:52 +0000

    paper and scissors

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -12,10 +12,16 @@ class ZebraController < ApplicationController
   end
 
   def paper
+    choices = ["rock", "paper", "scissors"]
+    index = rand(0..2)
+    @their_move = choices[index] 
     render({ :template => "game_templates/play_paper" })
   end
 
   def scissors
+    choices = ["rock", "paper", "scissors"]
+    index = rand(0..2)
+    @their_move = choices[index] 
     render({ :template => "game_templates/play_scissors" })
   end
 
